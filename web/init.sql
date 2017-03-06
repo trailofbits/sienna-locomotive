5f20af1cf1c653d84f261dd8f5d49c39cffa0db2
Douglas Goddard <douglas.gastonguay@gmail.com>
2017-03-06 15:11:01 -0800

    Adding SQL file.

---
+++ web/init.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS winafl_jobs (
+	name TEXT NOT NULL, 
+	command TEXT NOT NULL, 
+	file TEXT NOT NULL,
+	module TEXT, 
+	offset INT, 
+	nargs INT DEFAULT 8,
+	timeout TEXT DEFAULT "10000+"
+);
+
+-- job_types:
+-- 0 = winafl
+CREATE TABLE IF NOT EXISTS runs (
+	job_id INT NOT NULL,
+	job_type INT NOT NULL,
+	start_time INT,
+	end_time INT,
+	time_limit INT DEFAULT 5 NOT NULL, -- minutes
+	crashes INT DEFAULT 0 NOT NULL,
+	hangs INT DEFAULT 0 NOT NULL,
+	in_dir TEXT NOT NULL,
+	out_dir TEXT NOT NULL
+	-- Max run
+	-- Crashes
+	-- Hangs
+);
+
+CREATE TABLE IF NOT EXISTS run_crash_link_table (
+	run_id INT NOT NULL,
+	crash_id INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS crashes (
+	sha256 TEXT NOT NULL,
+	location TEXT NOT NULL
+);
