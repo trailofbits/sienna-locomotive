72fdbc1e52aea4fc9d7806d594c06318650cb0a1
Douglas Goddard <douglas.gastonguay@gmail.com>
2017-07-19 18:28:36 -0400

    Add file write to prep the tests, read 8 tainted bytes from file for read_t and read_null_t.

--- triage/corpus/asm/build.sh
+++ triage/corpus/asm/build.sh
@@ -1,1 +1,2 @@
-yasm -f elf64 -p nasm test.asm -a x86 ; ld test.o -o test
+yasm -f elf64 test.asm; clang-3.8 -o test test.o
+# yasm -f elf64 -p nasm test.asm -a x86 ; ld test.o -o test
--- triage/corpus/asm/test.asm
+++ triage/corpus/asm/test.asm
@@ -1,34 +1,184 @@
-bits 64
+section .data
+    author      db 'TACIXAT',0x0a
+    author_len  equ $-author
+    arg_err     db 'error: invalid test number',0x0a
+    arg_len     equ $-arg_err
+    use_err     db 'USAGE: ./test TEST_NUMBER',0x0a
+    use_len     equ $-use_err
+    tmpfile     db '/tmp/crash_scratch',0x00
+    tmplen      equ $-tmpfile
+    null_data   db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0  ; 16
+    null_len    equ $-null_data
+    aaaa_data   db "AAAAAAAAAAAAAAAA"               ; 16
+    aaaa_len    equ $-aaaa_data
 
 section .text
-global _start 
+; global _start 
+extern printf
+extern open
+extern malloc
+extern free
 
-_start:
-    pop     rcx
-    mov     rax, rsp
-    call    main
-
+global main
 main:
 ;   args:
-;       rcx, argc
-;       rax, argv
+;       rdi, argc
+;       rsi, argv
 ;   rets:   none
     push    rbp
     mov     rbp, rsp
-    push    rcx
-    push    rax
+    mov     rcx, rdi
+    mov     rax, rsi
     call    parse_args
     cmp     rax, 0
-    je      indirect_test
-    call    read_8
-    call    write_8
+    je      test_indirect
+    cmp     rax, 1
+    je      test_read_null_nt
+    cmp     rax, 2
+    je      test_read_null_t
+    cmp     rax, 3
+    je      test_read_t
     xor     rax, rax
-    call    indirect
-finish:
+main_finish:
     call    exit
-indirect_test:
+test_indirect:
     call    indirect
-    jmp     finish
+    jmp     main_finish
+test_read_null_nt:
+    call    read_null_nt
+    jmp     main_finish
+test_read_null_t:
+    call    read_null_t
+    jmp     main_finish
+test_read_t:
+    call    read_t
+    jmp     main_finish
+
+global pret_test
+prep_test:
+;   args:   
+;       rdi, filename
+;       rsi, data to write
+;       rdx, data length
+;   rets:
+;       none
+    push    rbp
+    mov     rbp, rsp
+    push    rsi
+    push    rdx
+    mov     rax, 2          ; open
+    mov     rdi, tmpfile    ; file
+    mov     rsi, 1102o      ; O_CREAT | O_TRUNC | O_WRONLY
+    mov     rdx, 664o       ; persimmons
+    syscall
+    pop     rdx             ; data len
+    pop     rdi             ; data
+    push    rax             ; fd
+    mov     rax, rdi
+    mov     rcx, rdx
+    pop     rdx
+    push    rdx             ; fd
+    call    write_fd
+    pop     rdi             ; fd
+    mov     rax, 3
+    syscall
+    mov     rsp, rbp
+    pop     rbp
+    ret
+
+read_file_8:
+;   args:   none
+;   rets:   rax, 8 bytes read
+    push    rbp
+    mov     rbp, rsp
+    ; open
+    mov     rax, 2          ; open
+    mov     rdi, tmpfile    ; file
+    mov     rsi, 0          ; O_RDONLY
+    mov     rdx, 0          ; persimmons
+    syscall
+    push    rax             ; fd
+    ; read
+    mov     rdi, rax        ; fd
+    xor     rax, rax        ; read
+    sub     rsp, 8          
+    mov     rsi, rsp        ; buf
+    mov     rdx, 8          ; size
+    syscall
+    pop     rax             ; data
+    ; close
+    pop     rdi             ; fd
+    push    rax
+    mov     rax, 3          ; close
+    syscall
+    pop     rax
+    mov     rsp, rbp
+    pop     rbp
+    ret
+
+global read_null_nt
+read_null_nt:
+;   args:   none
+;   rets:   none
+    xor     rax, rax
+    mov     rbx, [rax]
+
+global read_nt
+read_nt:
+;   args:   none
+;   rets:   none
+    mov     rax, 0x4141414141414141
+    mov     rbx, [rax]
+
+global read_null_t
+read_null_t:
+;   args:   none
+;   rets:   none
+    mov     rdi, tmpfile
+    mov     rsi, null_data
+    mov     rdx, null_len
+    call    prep_test
+    call    read_file_8
+    ; xor     rax, rax
+    mov     rbx, [rax]
+
+global read_t
+read_t:
+;   args:   none
+;   rets:   none
+    mov     rdi, tmpfile
+    mov     rsi, aaaa_data
+    mov     rdx, aaaa_len
+    call    prep_test
+    call    read_file_8
+    mov     rbx, [rax]
+
+global read_null_nt
+write_null_nt:
+;   args:   none
+;   rets:   none
+    xor     rax, rax
+    mov     [rax], rbx
+
+write_nt:
+;   args:   none
+;   rets:   none
+    mov     rax, 0x4141414141414141
+    mov     [rax], rbx
+
+
+write_null_t:
+;   args:   none
+;   rets:   none
+    call    read_8
+    xor     rax, rax
+    mov     [rax], rbx
+
+write_t:
+;   args:   none
+;   rets:   none
+    call    read_8
+    mov     [rax], rbx
 
 parse_args:
 ;   args:
@@ -39,15 +189,25 @@ parse_args:
     mov     rbp, rsp
     cmp     rcx, 2
     jl      invalid_use
-    mov     rax, [rax+8]    ; argv[1]
-    xor     rbx, rbx
-    mov     bl, [rax]       ; argv[1][0]
+    mov     rsi, [rax+8]    ; argv[1]
+    xor     rdi, rdi        ; total
+    xor     rcx, rcx        ; count
+    xor     rbx, rbx        ; current chr
+parse_loop:
+    mov     bl, [rsi+rcx]   ; argv[1][0]
+    cmp     bl, 0
+    je      parse_args_finish
     sub     rbx, 0x30       ; unasciify
     cmp     rbx, 0          ; lower bound
     jl      invalid_arg
-    cmp     rbx, 0          ; upper bound
+    cmp     rbx, 9          ; upper bound
     jg      invalid_arg
-    mov     rax, rbx        ; return
+    imul    rdi, 10
+    add     rdi, rbx
+    inc     rcx
+    jmp     parse_loop
+parse_args_finish:
+    mov     rax, rdi        ; ret    
     mov     rsp, rbp
     pop     rbp
     ret
@@ -108,14 +268,44 @@ write:
 ;   rets:   none
     push    rbp
     mov     rbp, rsp
-    mov     rsi, rax
-    mov     rdx, rcx
-    mov     rdi, 1      ; stdout
+    mov     rdx, 1
+    call    write_fd
+    mov     rsp, rbp
+    pop     rbp
+    ret    
+
+write_fd:    
+;   args:   
+;       rax, bytes to write
+;       rcx, length
+;       rdx, fd
+;   rets:   none
+    push    rbp
+    mov     rbp, rsp
+    mov     rdi, rdx    ; fd
+    mov     rsi, rax    ; buf
+    mov     rdx, rcx    ; count
     mov     rax, 1      ; write
     syscall
     mov     rsp, rbp
     pop     rbp
     ret    
+
+write_stdin:    
+;   args:   
+;       rax, bytes to write
+;       rcx, length
+;   rets:   none
+    push    rbp
+    mov     rbp, rsp
+    mov     rsi, rax
+    mov     rdx, rcx
+    mov     rdi, 0      ; stdout
+    mov     rax, 1      ; write
+    syscall
+    mov     rsp, rbp
+    pop     rbp
+    ret  
 
 exit:
 ;   args:   rax, exit code
@@ -125,11 +315,3 @@ exit:
     mov     rdi, rax    ; code
     mov     rax, 0x3c   ; exit
     syscall
-
-section .data
-    author      db 'TACIXAT',0x0a
-    author_len  equ $-author
-    arg_err     db 'error: invalid test number',0x0a
-    arg_len     equ $-arg_err
-    use_err     db 'USAGE: ./test TEST_NUMBER',0x0a
-    use_len     equ $-use_err
