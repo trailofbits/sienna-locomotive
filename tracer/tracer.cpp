5fe458551b960e50d39a73f8cf6e6d831d408264
Douglas Gastonguay-Goddard <douglas (dot) gastonguay (at) gmail (dot) com>
2017-11-15 17:07:00 -0500

    Add xed_decode to get instruction length.

--- tracer/tracer.cpp
+++ tracer/tracer.cpp
@@ -4,6 +4,11 @@
 #include "stdafx.h"
 #include "Windows.h"
 #include <list>
+#include <map>
+extern "C" {
+#include "include/xed-interface.h"
+}
+
 /*
 Trace:
 	Single step instructions
@@ -14,32 +19,101 @@ Trace:
 	Log address, opcode, and length (for Triton inst)
 */
 
-int trap_card(std::list<HANDLE> threads) {
-	std::list<HANDLE>::iterator threadIt;
+/*
+Profiler:
+	Break on start
+	Restore breakpoint
+loop:
+	Check have basic block at address
+	If have address:
+		Set break on end of bb
+		Record trace (bb)
+		Restore breakpoint
+		Set single step for branch
+	Don't have address:
+		Single step until control flow insn 
+		OR Walk forward through memory until control flow insn
+		Store bb by address
+		Record trace (bb)
+		Set single step for branch
+	
+	GOTO loop
+
+*/
+
+std::map<LPVOID, BYTE> restoreBytes;
+
+int singleStep(HANDLE hThread) {
 	CONTEXT context;
-	
-	for (threadIt = threads.begin(); threadIt != threads.end(); threadIt++) {
-		context.ContextFlags = CONTEXT_CONTROL;
-		GetThreadContext(*threadIt, &context);
-		context.EFlags = context.EFlags | (1 << 8);
-		if (!SetThreadContext(*threadIt, &context)) {
-			printf("ERROR: SetThreadContext (%x)\n", GetLastError());
-		}
+	context.ContextFlags = CONTEXT_CONTROL;
+	SuspendThread(hThread);
+	GetThreadContext(hThread, &context);
+	context.EFlags = context.EFlags | (1 << 8);
+	if (!SetThreadContext(hThread, &context)) {
+		printf("ERROR: SetThreadContext (%x)\n", GetLastError());
+		return 1;
 	}
-	
+	ResumeThread(hThread);
 	return 0;
+}
+
+int setBreak(CREATE_PROCESS_DEBUG_INFO cpdi) {
+	BYTE breakByte = 0xCC;
+	BYTE startByte;
+	ReadProcessMemory(cpdi.hProcess, cpdi.lpStartAddress, &startByte, sizeof(BYTE), NULL);
+	WriteProcessMemory(cpdi.hProcess, cpdi.lpStartAddress, &breakByte, sizeof(BYTE), NULL);
+	restoreBytes[cpdi.lpStartAddress] = startByte;
+	return 0;
+}
+
+int restoreBreak(HANDLE hProcess, HANDLE hThread) {
+	CONTEXT context;
+	context.ContextFlags = CONTEXT_ALL;
+	GetThreadContext(hThread, &context);
+	printf("RIP: %x\n", context.Rip);
+	context.Rip -= 1;
+	SetThreadContext(hThread, &context);
+	
+	WriteProcessMemory(hProcess, (LPVOID)context.Rip, &restoreBytes[(LPVOID)context.Rip], sizeof(BYTE), NULL);
+
+	return 0;
+}
+
+int getLength(HANDLE hProcess, PVOID address) {
+	xed_decoded_inst_t xedd;
+	BYTE buf[15];
+	xed_error_enum_t xed_err;
+	ReadProcessMemory(hProcess, address, buf, 15, NULL);
+	
+	xed_decoded_inst_zero(&xedd);
+	xed_decoded_inst_set_mode(&xedd, XED_MACHINE_MODE_LONG_64, XED_ADDRESS_WIDTH_64b);
+	xed_err = xed_decode(&xedd, buf, 15);
+
+	//printf("INSN: %x %x\n", address, xedd._decoded_length);
+	//for (int i = 0; i < 15; i++) {
+	//	printf("%x ", buf[i]);
+	//}
+	//printf("\n");
+	//printf("ERR: %s\n", xed_error_enum_t2str(xed_err));
+	//
+	//CHAR outBuf[1024];
+	////xed_decoded_inst_dump(&xedd, outBuf, 1024);
+	//xed_format_context(XED_SYNTAX_INTEL, &xedd, outBuf, 1024, 0, 0, 0);
+	//printf("%s\n\n", outBuf);
+
+	return xedd._decoded_length;
 }
 
 int debug_main_loop() {
 	DWORD dwContinueStatus = DBG_CONTINUE;
 	CREATE_PROCESS_DEBUG_INFO cpdi;
-	std::list<HANDLE> threads;
-	UINT64 count = 0;
+	std::map<DWORD, HANDLE> threadMap;
+	xed_tables_init();
 	for (;;)
 	{
 		DEBUG_EVENT dbgev;
-
 		WaitForDebugEvent(&dbgev, INFINITE);
+
 		//printf("DEBUG EVENT: %d\n", dbgev.dwDebugEventCode);
 
 		switch (dbgev.dwDebugEventCode)
@@ -55,58 +129,51 @@ int debug_main_loop() {
 				break;
 			case EXCEPTION_BREAKPOINT:
 				printf("BREAK AT %x\n", address);
-				//threads.clear();
-				//threads.push_back(cpdi.hThread);
-				trap_card(threads);
-				break;
-			case EXCEPTION_DATATYPE_MISALIGNMENT:
-				printf("DATA AT %x\n", address);
-				break;
-			case EXCEPTION_SINGLE_STEP:
-				//printf("SINGLE STEP AT %x\n", address);
-				count++;
-				trap_card(threads);
-				if (count % 10000 == 0) {
-					printf("COUNT: %x\n", count);
+				if (address == cpdi.lpStartAddress) {
+					printf("!!! AT START: %x\n", address);
+					restoreBreak(cpdi.hProcess, threadMap[dbgev.dwThreadId]);
+					singleStep(threadMap[dbgev.dwThreadId]);
+					getLength(cpdi.hProcess, address);
 				}
 				break;
+			case EXCEPTION_DATATYPE_MISALIGNMENT:
+				break;
+			case EXCEPTION_SINGLE_STEP:
+				singleStep(threadMap[dbgev.dwThreadId]);
+				getLength(cpdi.hProcess, address);
+				break;
 			case DBG_CONTROL_C:
-				printf("DBG AT %x\n", address);
 				break;
 			default:
 				break;
 			}
 			break;
 		case CREATE_THREAD_DEBUG_EVENT:
-			threads.push_back(dbgev.u.CreateThread.hThread);
+			threadMap[dbgev.dwThreadId] = dbgev.u.CreateThread.hThread;
+			//trap_card(threadMap);
 			printf("CREATE THREAD\n");
 			break;
 		case CREATE_PROCESS_DEBUG_EVENT:
 			printf("CREATE PROC\n");
 			cpdi = dbgev.u.CreateProcessInfo;
-			//DebugBreakProcess(cpdi.hProcess);
-			// TODO: set the trap eflag every instruction ?
+			setBreak(cpdi);
 			break;
 		case EXIT_THREAD_DEBUG_EVENT:
 			printf("EXIT THREAD\n");
+			threadMap.erase(dbgev.dwThreadId);
 			break;
 		case EXIT_PROCESS_DEBUG_EVENT:
-			printf("EXIT PROC (%x)\n", count);
+			printf("EXIT PROC\n");
 			EXIT_PROCESS_DEBUG_INFO epdi = dbgev.u.ExitProcess;
 			exit(0);
 			break;
 		case LOAD_DLL_DEBUG_EVENT:
-			printf("LOAD DLL\n");
-			LOAD_DLL_DEBUG_INFO lddi = dbgev.u.LoadDll;
 			break;
 		case UNLOAD_DLL_DEBUG_EVENT:
-			printf("UNLOAD DLL\n");
 			break;
 		case OUTPUT_DEBUG_STRING_EVENT:
-			printf("OUT DBG\n");
 			break;
 		case RIP_EVENT:
-			printf("RIP\n");
 			break;
 		}
 
@@ -159,6 +226,9 @@ int main()
 		printf("ERROR: CreateProcess (%x)\n", GetLastError());
 		return 1;
 	}
+
+	DebugBreakProcess(pi.hProcess);
+	//trap_card(threadMap);
 
 	ResumeThread(pi.hThread);
 	debug_main_loop();
