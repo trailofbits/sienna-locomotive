cfedd9c20874a5663567c4eb514074e4a33a741f
Car Bauer <car@trailofbits.com>
2018-01-30 10:44:31 -0800

    Adding skeleton files for DynamoRIO-based fuzzer

---
+++ fuzz_dynamorio/fuzzer.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "dr_api.h"
+#include "drmgr.h"
+
+DR_EXPORT void dr_client_main(client_id_t id, int argc, const char *argv[]) {
+    dr_set_client_name("Sienna-Locomotive Fuzzer",
+                       "https://github.com/trailofbits/sienna-locomotive/issues");
+}
