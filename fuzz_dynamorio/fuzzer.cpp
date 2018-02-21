b8f53187d51b1b3d7a41f6521a04efc065434dc3
car <car@trailofbits.com>
2018-02-21 13:18:03 -0500

    Replacing buffers for ReadFile, ReadFileEx with As

--- fuzz_dynamorio/fuzzer.cpp
+++ fuzz_dynamorio/fuzzer.cpp
@@ -23,9 +23,8 @@ static void event_exit(void);
 static void wrap_pre_ReadFile(void *wrapcxt, OUT void **user_data);
 static void wrap_post_ReadFile(void *wrapcxt, void *user_data);
 
-static size_t max_malloc;
-static uint malloc_oom;
-static void *max_lock; /* sync writes to max_malloc */
+static size_t max_ReadFile;
+static void *max_lock; /* sync writes to max_ReadFile */
 
 static BOOL mutate(HANDLE hFile, DWORD64 position, LPVOID buf, DWORD size);
 
@@ -49,6 +48,15 @@ module_load_event(void *drcontext, const module_data_t *mod, bool loaded) {
 		    dr_fprintf(STDERR, "<FAILED to wrap ReadFile @ 0x%p: already wrapped?\n", towrap);
 	    }
     }
+    towrap = (app_pc) dr_get_proc_address(mod->handle, "ReadFileEx");
+    if (towrap != NULL) {
+	    bool ok = drwrap_wrap(towrap, wrap_pre_ReadFile, wrap_post_ReadFile);
+	    if (ok) {
+		    dr_fprintf(STDERR, "<wrapped ReadFileEx @ 0x%p\n", towrap);
+	    } else {
+		    dr_fprintf(STDERR, "<FAILED to wrap ReadFileEx @ 0x%p: already wrapped?\n", towrap);
+	    }
+    }
 }
 
 
@@ -65,63 +73,94 @@ DR_EXPORT void dr_client_main(client_id_t id, int argc, const char *argv[]) {
 
     drmgr_init();
     drwrap_init();
+    
+    max_ReadFile = 0;
+    
     dr_register_exit_event(event_exit);
     drmgr_register_module_load_event(module_load_event);
-    max_lock = dr_mutex_create();
 }
 
 /* from wrap.cpp sample code */
 static void
 event_exit(void) {
-    char msg[256];
-    int len;
-    len = dr_snprintf(msg, sizeof(msg)/sizeof(msg[0]),
-		    "<Largest ReadFile request: %d>\n<OOM simulations: %d>\n",
-		    max_malloc, malloc_oom);
-    DR_ASSERT(len > 0);
-    NULL_TERMINATE(msg);
-    DISPLAY_STRING(msg);
+    //char msg[256];
+    //int len;
+    //len = dr_snprintf(msg, sizeof(msg)/sizeof(msg[0]),
+	//	    "<Largest ReadFile request: %d>\n",
+	//	    max_ReadFile);
+    //DR_ASSERT(len > 0);
+    //NULL_TERMINATE(msg);
+    //DISPLAY_STRING(msg);
 
-    dr_mutex_destroy(max_lock);
     drwrap_exit();
     drmgr_exit();
 }
 
 
 DWORD64 position = 0;
-
+DWORD   nNumberOfBytesToRead;
+LPDWORD lpNumberOfBytesRead;
+HANDLE  hFile;
 /* from wrap.cpp sample code */
 static void
 wrap_pre_ReadFile(void *wrapcxt, OUT void **user_data) {
-	dr_fprintf(STDERR, "In wrap_pre_ReadFile\n");
-    
-    HANDLE hFile = drwrap_get_arg(wrapcxt, 0);
+    hFile =                drwrap_get_arg(wrapcxt, 0);
+    LPVOID lpBuffer =      drwrap_get_arg(wrapcxt, 1);
+    nNumberOfBytesToRead = (DWORD)drwrap_get_arg(wrapcxt, 2);
+    lpNumberOfBytesRead =  (LPDWORD)drwrap_get_arg(wrapcxt, 3);
+    //if (max_ReadFile < nNumberOfBytesToRead) {
+    //    max_ReadFile = nNumberOfBytesToRead;
+    //}
+    *user_data = lpBuffer;
 
     LONG positionHigh = 0;
 
     // we may need to go lower-level than just calling windows api?
-    DWORD positionLow = SetFilePointer(hFile, 0, &positionHigh, FILE_CURRENT);
-    DWORD64 position = positionHigh;
-    position = (position << 32) | positionLow;
+    //DWORD positionLow = SetFilePointer(hFile, 0, &positionHigh, FILE_CURRENT);
+    //DWORD64 position = positionHigh;
+    //position = (position << 32) | positionLow;
 }
 
 static void
 wrap_post_ReadFile(void *wrapcxt, void *user_data) {
-	dr_fprintf(STDERR, "In wrap_post_ReadFile\n");
-    HANDLE hFile =               (HANDLE)drwrap_get_arg(wrapcxt, 0);
-    LPVOID lpBuffer =            drwrap_get_arg(wrapcxt, 1);
-    DWORD nNumberOfBytesToRead = (DWORD)drwrap_get_arg(wrapcxt, 2);
-    LPDWORD lpNumberOfBytesRead = (LPDWORD)drwrap_get_arg(wrapcxt, 3);
+    LPVOID lpBuffer = user_data;
+    DWORD nNumberOfBytesToRead;
+	if (lpNumberOfBytesRead) {
+        nNumberOfBytesToRead = *lpNumberOfBytesRead;
+    }
+    
+    //dr_fprintf(STDERR, "In wrap_post_ReadFile (%d)\n", hFile);
+    //dr_printf("post- lpBuffer %p: %s\n", lpBuffer, lpBuffer);
    
     if (!replay && !trace || replay) {
-        if (!mutate(hFile, position, lpBuffer, nNumberOfBytesToRead)) {
+        //if (!mutate(hFile, position, lpBuffer, nNumberOfBytesToRead)) {
+        if (!mutate(hFile, position, lpBuffer, *lpNumberOfBytesRead)) {
             // TODO: fallback mutations?
+            dr_printf("failed to mutate lpBuffer\n");
         }
     }
+    
+    //TCHAR *new_buf = (TCHAR *)lpBuffer;
+    //for(DWORD i = 0; i < nNumberOfBytesToRead; ++i) {
+    //    if (isprint(new_buf[i])) {
+    //        dr_printf("%c", new_buf[i]);
+    //    }
+    //    else {
+    //        dr_printf(".");
+    //    }
+    //    new_buf[i] = (TCHAR)'A';
+    //    dr_printf("%c", new_buf[i]);
+    //}
+
+    //dr_printf("mutated lpBuffer %p: %s", lpBuffer, lpBuffer);
 
     if (lpNumberOfBytesRead != NULL) {
         *lpNumberOfBytesRead = nNumberOfBytesToRead;
     }
+    
+    //dr_printf("lpNumberOfBytesRead = %d\n", *lpNumberOfBytesRead);
+
+    drwrap_set_mcontext(wrapcxt);
 
     BOOL ok = TRUE; 
     drwrap_set_retval(wrapcxt, &ok); // FIXME
@@ -130,7 +169,20 @@ wrap_post_ReadFile(void *wrapcxt, void *user_data) {
 static DWORD mutateCount = 0;
 static BOOL mutate(HANDLE hFile, DWORD64 position, LPVOID buf, DWORD size) {
 	TCHAR filePath[MAX_PATH+1];
+    TCHAR *new_buf = (TCHAR *)buf;
+    
+    for(DWORD i = 0; i < size; ++i) {
+        if (isprint(new_buf[i])) {
+            dr_printf("%c", new_buf[i]);
+        }
+        else {
+            dr_printf(".");
+        }
+        new_buf[i] = (TCHAR)'A';
+        dr_printf("%c", new_buf[i]);
+    }
 
+    /*
 	DWORD pathSize = GetFinalPathNameByHandle(hFile, filePath, MAX_PATH, 0);
 
 	if (pathSize > MAX_PATH || pathSize == 0) {
@@ -185,7 +237,9 @@ static BOOL mutate(HANDLE hFile, DWORD64 position, LPVOID buf, DWORD size) {
 		TransactNamedPipe(hPipe, &size, sizeof(DWORD), buf, size, &bytesRead, NULL);
 	}
 	CloseHandle(hPipe);
-	mutateCount++;
+	
+    */
+    mutateCount++;
 
 	return true;
 }
