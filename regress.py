e55921069eb6cb75dfef9d82fc8fda1edcc82eda
Weston Hopkins <haxmeadroom@gmail.com>
2018-07-11 22:46:02 -0700

    more wizard regression tests

--- regress.py
+++ regress.py
@@ -5,8 +5,26 @@ import sys
 import subprocess
 import shlex
 
+
+def runAndCaptureOutput( cmd ):
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,  stderr=subprocess.PIPE, close_fds=True)
+    stdout, stderr = p.communicate()
+    stdout = str(stdout)
+    stderr = str(stderr)
+    return stdout, stderr
+
 class TestWizard(unittest.TestCase):
     
+    
+    def test_noargs(self):
+        cmd =  [ r'python.exe',
+            r'harness.py' ]
+        
+        stdout, stderr = runAndCaptureOutput(cmd)
+        self.assertTrue( 'caused EXCEPTION_ACCESS_VIOLATION'  in stdout )
+        self.assertRegex(  stdout,  r"0x[a-f0-9]+: mov.*%rax.*-> %edx" )
+
+
     def test_0(self):
         
         cmd =  [ r'.\dynamorio\bin64\drrun.exe',
@@ -16,9 +34,7 @@ class TestWizard(unittest.TestCase):
             r'build\corpus\test_application\Debug\test_application.exe',
             r'0' ]
         
-        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,  stderr=subprocess.PIPE, close_fds=False)
-        stdout, stderr = p.communicate()
-        stderr = str(stderr)
+        stdout, stderr = runAndCaptureOutput(cmd)
         self.assertTrue( 'aa8b10f2e5498367555fe6f09175f4d89f93cfe4d4af736cc5245cb8ac7ba1e9'  in stderr )
         self.assertTrue(  r'buffer":[65,65,65,65,65,65,65,65]'  in stderr )
 
@@ -31,11 +47,8 @@ class TestWizard(unittest.TestCase):
             r'build\corpus\test_application\Debug\test_application.exe',
             r'2' ]
         
-        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,  stderr=subprocess.PIPE, close_fds=False)
-        stdout, stderr = p.communicate()
-        stderr = str(stderr)
+        stdout, stderr = runAndCaptureOutput(cmd)
         self.assertTrue(  r'[60,104,116,109,108,62,10,32]' in stderr )
-
 
 
 def main():
