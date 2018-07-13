14aef208658c69e1cbf3fb19bb0a064b04c0b408
Eric Hennenfent <eric.hennenfent@trailofbits.com>
2018-07-13 16:19:42 -0400

    Signal Handler for checked/unchecked

--- gui.py
+++ gui.py
@@ -1,9 +1,10 @@
 import sys
-import json
 
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtGui import QFontDatabase
+
+from gui.checkbox_tree import CheckboxTreeWidget, CheckboxTreeWidgetItem
 
 from harness import config
 from harness.state import get_target
@@ -27,23 +28,28 @@ class MainWindow(QtWidgets.QMainWindow):
 
         target_data = get_target(config.config)
 
-        self._func_tree = QtWidgets.QTreeWidget()
+        self._func_tree = CheckboxTreeWidget()
         self._layout.addWidget(self._func_tree)
 
+        self._func_tree.itemCheckedStateChanged.connect(self.changed)
+
         for option in target_data:
-            widget = QtWidgets.QTreeWidgetItem(self._func_tree)
-            widget.setCheckState(0, Qt.Checked if option["selected"] else Qt.Unchecked)
+            widget = CheckboxTreeWidgetItem(self._func_tree)
             widget.setText(0, ("{func_name} from {source}:{start}-{end}" if 'source' in option
                                else "{func_name}").format(**option))
+            widget.setCheckState(0, Qt.Checked if option["selected"] else Qt.Unchecked)
 
             for address in range(0, min(len(option["buffer"]), 16*5), 16):
                 hexstr = " ".join("{:02X}".format(c) for c in option["buffer"][address:address + 16])
                 asciistr = "".join((chr(c) if c in range(31, 127) else '.') for c in option["buffer"][address:address + 16])
                 formatted = "0x%04X:  %s  | %s" % (address, hexstr + " " * (16 * 3 - len(hexstr)), asciistr)
-                data_disp = QtWidgets.QTreeWidgetItem(widget, [formatted])
+                data_disp = CheckboxTreeWidgetItem(widget, [formatted])
                 data_disp.setFont(0, QFontDatabase.systemFont(QFontDatabase.FixedFont))
 
             self._func_tree.insertTopLevelItem(0, widget)
+
+    def changed(self, widget, column, is_checked):
+        print(widget.text(column), "Checked" if is_checked else "Unchecked")
 
     def get_config(self):
         profile, cont = QtWidgets.QInputDialog.getItem(self,
