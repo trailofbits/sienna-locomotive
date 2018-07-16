f113f60b42793831085cd43195f7011466560aed
William Woodruff <william.woodruff@trailofbits.com>
2018-07-16 16:39:20 -0400

    uuid: explain SL2_UUID_SIZE

--- include/common/uuid.h
+++ include/common/uuid.h
@@ -4,6 +4,7 @@
 #include <Windows.h>
 #include <Rpc.h>
 
+// 32 hexadecimal characters, 4 dashes, and a NULL.
 #define SL2_UUID_SIZE (37)
 #define SL2_UUID_FMT (L"%08x-%04hx-%04hx-%02hhx%02hhx-%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx")
 
