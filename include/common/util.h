4a6b0c808f6905f8cf0531921eaf737ce1ffbe07
William Woodruff <william.woodruff@trailofbits.com>
2018-07-30 11:00:38 -0400

    server: use SL2_ARGV_LEN instead of hardcoding
    Fix overrun checks to account for widechars.

--- include/common/util.h
+++ include/common/util.h
@@ -8,6 +8,12 @@
 // The size of a SHA256 hash.
 #define SL2_HASH_LEN 64
 
+// The maximum length of a target application's arguments (including program name and NULL).
+// NOTE(ww): This is based on the maximum argument length on the command line,
+// *not* the maximum length accepted by either CreateProcess or other WinAPI functions.
+// As such, it's mostly arbitrary.
+#define SL2_ARGV_LEN 8192
+
 // Convenience macros for testing normal and wide strings
 // for equality and case equality.
 #define STREQ(a, b) (!strcmp(a, b))
