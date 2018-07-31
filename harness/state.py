023e06da47363f1c07a0d50474d87df2c88a42e9
William Woodruff <william.woodruff@trailofbits.com>
2018-07-31 17:09:35 -0400

    harness/state: flake8 linting

--- harness/state.py
+++ harness/state.py
@@ -63,19 +63,28 @@ def create_invocation_statement(config_dict):
     return tup
 
 
-# TODO(ww): Use shlex or something similar here.
 def stringify_program_array(target_application_path, target_args_array):
-    """ Escape paths with spaces in them by surrounding them with quotes """
-    out = "{} {}\n".format(esc_quote(target_application_path), ' '.join(esc_quote(k) for k in target_args_array))
+    """
+    Escape paths with spaces in them by surrounding them with quotes.
+    """
+    out = "{} {}\n".format(
+        esc_quote(target_application_path),
+        ' '.join(esc_quote(k) for k in target_args_array)
+    )
+
     return out
 
 
 # TODO: Use shlex or something similar here.
 def unstringify_program_array(stringified):
-    """ Turn a stringified program array back into the tokens that went in. Treats quoted entities as atomic,
-         splits all others on spaces. """
+    """
+    Turn a stringified program array back into the tokens that went in.
+    Treats quoted entities as atomic,
+    splits all others on spaces.
+    """
     invoke = []
-    split = re.split('(\".*?\")', stringified)  # TODO use this for config file parsing
+    # TODO use this for config file parsing
+    split = re.split('(\".*?\")', stringified)
     for token in split:
         if "\"" in token:
             invoke.append(token)
@@ -87,7 +96,11 @@ def unstringify_program_array(stringified):
 
 
 def get_target_dir(_config):
-    """ Gets (or creates) the path to a target directory for the current config file """
+    """
+    Gets (or creates) the path to a target directory for the current
+    config file.
+    """
+    # TODO(ww): Use os.path.basename for this?
     exe_name = _config['target_application_path'].split('\\')[-1].strip('.exe').upper()
     dir_hash = sha1("{} {}".format(_config['target_application_path'], _config['target_args']).encode('utf-8')).hexdigest()
     dir_name = os.path.join(config.sl2_targets_dir, "{}_{}".format(exe_name, dir_hash))
@@ -96,7 +109,10 @@ def get_target_dir(_config):
     arg_file = os.path.join(dir_name, 'arguments.txt')
     if not os.path.exists(arg_file):
         with open(arg_file, 'w') as argfile:
-            argfile.write(stringify_program_array(_config['target_application_path'], _config['target_args']))
+            argfile.write(
+                stringify_program_array(
+                    _config['target_application_path'],
+                    _config['target_args']))
     return dir_name
 
 
@@ -208,7 +224,11 @@ def parse_triage_output(run_id):
             formatted += ("\n\t0x{location:02x}: {instruction}".format(**results))
             return formatted, results
     except FileNotFoundError:
-        message = "The triage tool exited improperly during run {}, but no crash file could be found. It may have timed out. To retry it manually, run `python harness.py -v -e TRIAGE [-p <PROFILE>]`"
+        message = "The triage tool exited improperly during run {}, \
+        but no crash file could be found. It may have timed out. \
+        To retry it manually, run \
+        `python harness.py -v -e TRIAGE [-p <PROFILE>]`"
+
         return message.format(run_id), None
 
 
