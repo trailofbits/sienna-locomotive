f0dcdfbb6b5856eb08b934ca0fe19984fb585495
Eric Hennenfent <ehennenfent@imsa.edu>
2018-09-07 13:51:56 -0500

    Move missing comment

--- triage.py
+++ triage.py
@@ -14,6 +14,9 @@ from PySide2.QtWidgets import QFileDialog
 
 import db
 
+## strips out characters in a string that could be harmful for filenames or paths
+# @param s strings to sanitize
+# @return sanitized string
 def sanitizeString(s):
     ret = re.sub(r"[^a-zA-Z0-9._]+", "_", s)
     return re.sub(r"_+", "_", ret)
