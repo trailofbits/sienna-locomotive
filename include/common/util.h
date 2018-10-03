ca28471ee80e380e2da7a13aaab6ccefeae8e94c
Eric Hennenfent <ehennenfent@imsa.edu>
2018-10-03 14:37:37 -0500

    Doxygen format comments for helper headers

--- include/common/util.h
+++ include/common/util.h
@@ -5,22 +5,30 @@
 
 #define SL2_EXPORT __declspec(dllexport)
 
-// The number of mutation strategies currently implemented by SL2.
-// NOTE(ww): This MUST be kept up-to-date with the size of SL2_STRATEGY_TABLE in mutation.(cpp|hpp).
-// We define it here so that other (non-DR) components can us it (e.g., the server).
+
+/**
+ * The number of mutation strategies currently implemented by SL2.
+ *  NOTE(ww): This MUST be kept up-to-date with the size of SL2_STRATEGY_TABLE in mutation.(cpp|hpp).
+ *  We define it here so that other (non-DR) components can us it (e.g., the server).
+ */
 #define SL2_NUM_STRATEGIES 9
 
-// The size of a SHA256 hash.
+
+/**
+ * The size of a SHA256 hash.
+ */
 #define SL2_HASH_LEN 64
 
-// The maximum length of a target application's arguments (including program name and NULL).
-// NOTE(ww): This is based on the maximum argument length on the command line,
-// *not* the maximum length accepted by either CreateProcess or other WinAPI functions.
-// As such, it's mostly arbitrary.
+
+/**
+ * The maximum length of a target application's arguments (including program name and NULL).
+ *  NOTE(ww): This is based on the maximum argument length on the command line,
+ *  *not* the maximum length accepted by either CreateProcess or other WinAPI functions.
+ *  As such, it's mostly arbitrary.
+ */
 #define SL2_ARGV_LEN 8192
 
-// Convenience macros for testing normal and wide strings
-// for equality and case equality.
+/*! Convenience macros for testing normal and wide strings for equality and case equality. */
 #define STREQ(a, b) (!strcmp(a, b))
 #define STREQI(a, b) (!_stricmp(a, b))
 #define WSTREQ(a, b) (!wcscmp(a, b))
--- include/common/uuid.h
+++ include/common/uuid.h
@@ -6,21 +6,27 @@
 
 #include "common/util.h"
 
-// 32 hexadecimal characters, 4 dashes, and a NULL.
+/** 32 hexadecimal characters, 4 dashes, and a NULL. */
 #define SL2_UUID_SIZE (37)
 
 // TODO(ww): Remove this and use just the wide format above.
 #define SL2_UUID_FMT ("%08x-%04hx-%04hx-%02hhx%02hhx-%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx")
 
-// Transforms a UUID structure into a human-readable string.
-// NOTE(ww): This function exists because we can't load the Windows RPC
-// library that contains its equivalent within DynamoRIO clients.
+
+/**
+    Transforms a UUID structure into a human-readable string.
+    NOTE(ww): This function exists because we can't load the Windows RPC
+    library that contains its equivalent within DynamoRIO clients.
+ */
 SL2_EXPORT
 void sl2_uuid_to_string(UUID uuid, char dst[SL2_UUID_SIZE]);
 
-// Transforms a human-readable string into a UUID structure.
-// NOTE(ww): This function exists because we can't load the Windows RPC
-// library that contains its equivalent within DynamoRIO clients.
+
+/**
+    Transforms a human-readable string into a UUID structure.
+    NOTE(ww): This function exists because we can't load the Windows RPC
+    library that contains its equivalent within DynamoRIO clients.
+ */
 SL2_EXPORT
 void sl2_string_to_uuid(const char *uuid, UUID *dst);
 
