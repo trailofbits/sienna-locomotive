60920791b557ecfa03fb4da24c27c53c2530069e
William Woodruff <william@yossarian.net>
2018-11-30 10:38:21 -0500

    Revert "sl2: clang-format"

--- include/common/sl2_dr_client_options.hpp
+++ include/common/sl2_dr_client_options.hpp
@@ -7,12 +7,19 @@
 #include "droption.h"
 
 /*! Required, which specific call to target */
-static droption_t<std::string> op_target(DROPTION_SCOPE_CLIENT, "t", "", "target",
-                                         "Specific call to target.");
+static droption_t<std::string> op_target(
+    DROPTION_SCOPE_CLIENT,
+    "t",
+    "",
+    "target",
+    "Specific call to target.");
 
 /*! Enables Tracking of ReqQueryValueExA/W. Probably never needed for most workflows. */
-static droption_t<bool> op_registry(DROPTION_SCOPE_CLIENT, "registry", false,
-                                    "Enable Registry Tracking",
-                                    "Tracking of RegQuery*() functions");
+static droption_t<bool> op_registry (
+        DROPTION_SCOPE_CLIENT,
+        "registry",
+        false,
+        "Enable Registry Tracking",
+        "Tracking of RegQuery*() functions" );
 
 #endif
